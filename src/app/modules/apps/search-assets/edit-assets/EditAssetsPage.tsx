76fa0ef9c4c2c376c3e68722f47cfd20df8cd094
Yash-Wadhia-FIS <yash.wadhia@fisclouds.com>
2023-08-08 07:49:35 +0530

    feat(Sorting): Made album table inside edit assets tab sortable

--- src/app/modules/apps/search-assets/edit-assets/EditAssetsPage.tsx
+++ src/app/modules/apps/search-assets/edit-assets/EditAssetsPage.tsx
@@ -8,6 +8,7 @@ import {Metadata} from './components/Metadata'
 import {Settings} from './components/Settings'
 import { Albums } from './components/Albums'
 import {ProfileHeader} from './EditAssetsHeader'
+import { AlbumsTable } from './albums/AlbumsTable'
 
 
 const EditAssets = () => {
--- src/app/modules/apps/search-assets/edit-assets/components/Albums.tsx
+++ src/app/modules/apps/search-assets/edit-assets/components/Albums.tsx
@@ -1,10 +1,86 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
+import { useTable, useSortBy, Column } from 'react-table';
 
 import { KTIcon } from '../../../../../../_metronic/helpers'
 import { AlbumList } from '../../../albums/items/AlbumList'
+import { UserActionsCell } from '../albums/columns/UserActionsCell';
 
 export function Albums() {
+  const columns = React.useMemo<any>(
+    () => [
+      {
+        Header: "Album name",
+        accessor: "title",
+        Cell: ({...props}) => {
+          return(
+          <div className='d-flex align-items-center'>
+            <div className='symbol symbol-45px me-5'>
+              <KTIcon iconName='abstract-26' className='text-primary fs-1 me-5' />
+            </div>
+            <div className='d-flex justify-content-start flex-column'>
+              <a href='#' className='text-dark fw-bold text-hover-primary fs-6'>
+              {props.value}
+              </a>
+            </div>
+          </div>
+        )}
+      },
+      {
+        Header: "Date",
+        accessor: "date",
+        Cell: ({...props}) => {
+          return(
+            <span className='text-muted fw-semibold text-muted d-block fs-7'>
+            {props.value}
+          </span>
+        )}
+      },
+      {
+        Header: "Actions",
+        id: 'actions',
+        Cell: () => (
+          <div className='d-flex justify-content-end flex-shrink-0'>
+            <a
+              href='#'
+              className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
+            >
+              <KTIcon iconName='switch' className='fs-3' />
+            </a>
+            <a
+              href='#'
+              className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
+            >
+              <KTIcon iconName='pencil' className='fs-3' />
+            </a>
+            <a
+              href='#'
+              className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'
+            >
+              <KTIcon iconName='trash' className='fs-3' />
+            </a>
+          </div>
+        ),
+      }
+    ],
+    []
+  );
+
+  const data = React.useMemo(() => AlbumList, []);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useSortBy
+  );
+
   const [isUploadEnabled, setUploadEnabled] = useState(false);
 
   const handleCheckboxChange = () => {
@@ -19,6 +95,17 @@ export function Albums() {
 
     setUploadEnabled(checked);
   };
+
+  const getHeaderClass = (header: string) => {
+    switch (header) {
+      case "Album name":
+        return "min-w-150px";
+      case "Date":
+        return "min-w-140px";
+      case "Actions":
+        return "min-w-100px text-end";
+    }
+  }
   return (
     <>
       <div className='d-flex flex-wrap flex-stack mb-6'>
@@ -28,116 +115,42 @@ export function Albums() {
       </div>
       <div className={`card`}>
         {/* begin::Header */}
-        <div className='card-header border-0 pt-5'>
-          <h3 className='card-title align-items-start flex-column'>
-            <span className='card-label fw-bold fs-3 mb-1'></span>
-            <span className='text-muted mt-1 fw-semibold fs-7'></span>
-          </h3>
-          <div
-            className='card-toolbar'
-            data-bs-toggle='tooltip'
-            data-bs-placement='top'
-            data-bs-trigger='hover'
-            title='Click to add a user'
-          >
-            {isUploadEnabled && <a
-              href='#'
-              className='btn btn-sm btn-primary'
-            // data-bs-toggle='modal'
-            // data-bs-target='#kt_modal_invite_friends'
-            >
-              Upload
-            </a>}
-          </div>
-        </div>
+
         {/* end::Header */}
         {/* begin::Body */}
-        <div className='card-body py-3'>
+        <div className='card-body'>
           {/* begin::Table container */}
           <div className='table-responsive'>
-            {/* begin::Table */}
-            <table className='table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4'>
-              {/* begin::Table head */}
+            <table {...getTableProps()} className="table">
               <thead>
-                <tr className='fw-bold text-muted'>
-                  <th className='w-25px'>
-                    <div className='form-check form-check-sm form-check-custom form-check-solid'>
-                      <input
-                        className='form-check-input'
-                        type='checkbox'
-                        value='1'
-                        data-kt-check='true'
-                        data-kt-check-target='.widget-9-check'
-
-                      />
-                    </div>
-                  </th>
-                  <th className='min-w-150px'>Album name</th>
-                  <th className='min-w-140px'>Files</th>
-                  <th className='min-w-100px text-end'>Actions</th>
-                </tr>
-              </thead>
-              {/* end::Table head */}
-              {/* begin::Table body */}
-              <tbody>
-                {AlbumList.map((data, index) => (
-                  <tr>
-                    <td>
-                      <div className='form-check form-check-sm form-check-custom form-check-solid'>
-                        <input className='form-check-input widget-9-check' type='checkbox' value='1' onChange={handleCheckboxChange} />
-                      </div>
-                    </td>
-                    <td>
-                      <div className='d-flex align-items-center'>
-                        <div className='symbol symbol-45px me-5'>
-                          <KTIcon iconName='abstract-26' className='text-primary fs-1 me-5' />
-                        </div>
-                        <div className='d-flex justify-content-start flex-column'>
-                          <a href='#' className='text-dark fw-bold text-hover-primary fs-6'>
-                            {data.title}
-                          </a>
-                          <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                            {data?.date}
+                {headerGroups.map(headerGroup => (
+                  <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map((column: any) => {
+                      return (
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())} className={getHeaderClass(column.Header)}>
+                          {column.render('Header')}
+                          <span>
+                            {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
                           </span>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <a href='#' className='text-dark fw-bold text-hover-primary d-block fs-6'>
-                        Number of files
-                      </a>
-                      <span className='text-muted fw-semibold text-muted d-block fs-7'>
-                        {data?.files}
-                      </span>
-                    </td>
-                    <td>
-                      <div className='d-flex justify-content-end flex-shrink-0'>
-                        <a
-                          href='#'
-                          className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                        >
-                          <KTIcon iconName='switch' className='fs-3' />
-                        </a>
-                        <a
-                          href='#'
-                          className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1'
-                        >
-                          <KTIcon iconName='pencil' className='fs-3' />
-                        </a>
-                        <a
-                          href='#'
-                          className='btn btn-icon btn-bg-light btn-active-color-primary btn-sm'
-                        >
-                          <KTIcon iconName='trash' className='fs-3' />
-                        </a>
-                      </div>
-                    </td>
+                        </th>
+                      )
+                    })}
                   </tr>
                 ))}
+              </thead>
+              <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                  prepareRow(row);
+                  return (
+                    <tr {...row.getRowProps()}>
+                      {row.cells.map(cell => {
+                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                      })}
+                    </tr>
+                  );
+                })}
               </tbody>
-              {/* end::Table body */}
             </table>
-            {/* end::Table */}
           </div>
           {/* end::Table container */}
         </div>
